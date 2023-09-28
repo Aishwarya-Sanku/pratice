bc986a032f942d2b939d060433c75232a84f1ed6
Aishwarya <sanku.aishwarya@feuji.com>
2023-09-28 17:19:02 +0530

    jl

---
+++ h.py
@@ -0,0 +1,143 @@
+def join_with_alternating_parentheses(array, first_index, last_index):
+    result = ""
+
+    for i in range(first_index, last_index + 1):
+        value = array[i]
+        if (i - first_index) % 2 == 0:
+            result += value
+        else:
+            if i != last_index:
+                result += f"({value})."
+            else:
+                result += f"({value})"
+
+    return result
+
+
+
+def join_with_alternating_parentheses(array, first_index, last_index):
+    result = ""
+
+    for i in range(first_index, last_index + 1):
+        value = array[i]
+        if (i - first_index) % 2 == 0:
+            result += value
+        else:
+            if i != last_index:
+                result += f"({value})."
+            else:
+                result += f"({value})"
+
+    return result
+
+
+
+
+
+
+def join_with_alternating_parentheses(array, first_index, last_index):
+    result = ""
+
+    for i in range(first_index, last_index + 1):
+        value = array[i]
+        if (i - first_index) % 2 == 0:
+            result += value
+        else:
+            if i != last_index:
+                result += f"({value})."
+            else:
+                result += f"({value})"
+
+    return result
+
+
+
+
+
+def join_with_alternating_parentheses(array, first_index, last_index):
+    result = ""
+
+    for i in range(first_index, last_index + 1):
+        value = array[i]
+        if (i - first_index) % 2 == 0:
+            result += value
+        else:
+            if i != last_index:
+                result += f"({value})."
+            else:
+                result += f"({value})"
+
+    return result
+
+
+
+
+
+
+def join_with_alternating_parentheses(array, first_index, last_index):
+    result = ""
+
+    for i in range(first_index, last_index + 1):
+        value = array[i]
+        if (i - first_index) % 2 == 0:
+            result += value
+        else:
+            if i != last_index:
+                result += f"({value})."
+            else:
+                result += f"({value})"
+
+    return result
+
+
+
+
+
+def join_with_alternating_parentheses(array, first_index, last_index):
+    result = ""
+
+    for i in range(first_index, last_index + 1):
+        value = array[i]
+        if (i - first_index) % 2 == 0:
+            result += value
+        else:
+            if i != last_index:
+                result += f"({value})."
+            else:
+                result += f"({value})"
+
+    return result
+
+
+
+def join_with_alternating_parentheses(array, first_index, last_index):
+    result = ""
+
+    for i in range(first_index, last_index + 1):
+        value = array[i]
+        if (i - first_index) % 2 == 0:
+            result += value
+        else:
+            if i != last_index:
+                result += f"({value})."
+            else:
+                result += f"({value})"
+
+    return result
+
+
+
+def join_with_alternating_parentheses(array, first_index, last_index):
+    result = ""
+
+    for i in range(first_index, last_index + 1):
+        value = array[i]
+        if (i - first_index) % 2 == 0:
+            result += value
+        else:
+            if i != last_index:
+                result += f"({value})."
+            else:
+                result += f"({value})"
+
+    return result
